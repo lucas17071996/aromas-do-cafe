b7c849e7e25b4ccff139dbe21b8a89afc1b1c50b
Lucas Soares <156090735+lucas17071996@users.noreply.github.com>
2024-04-09 15:27:10 -0300

    Delete bootstrap=cafeteria directory

--- bootstrap=cafeteria/js/script.js
+++
@@ -1,6 +0,0 @@
-const inputCheck = document.querySelector('#dark-mode')
-const element = document.querySelector('body')
-inputCheck.addEventListener('click',() => {
-    const mode = inputCheck.checked ? 'dark': 'light'
-    element.setAttribute("data-bs-theme", mode)
-})
